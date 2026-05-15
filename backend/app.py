3ec54826aa7f93ae807a8bcc375cfb43cb61c09c
medi <saisuryawanshi0622@gamil.com>
2026-05-16 02:26:34 +0530

    error 3

--- backend/app.py
+++ backend/app.py
@@ -527,5 +527,5 @@ def export_excel():
     return jsonify({"error": "No data today"}), 404
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
-    app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
